e005f76901b84df7c6e14e7e9edccbcb339da7a1
Lv_Xinyan <meetlxy@outlook.com>
2019-06-23 14:48:29 +0800

    Update playlist.js

--- Song List/playlist.js
+++ Song List/playlist.js
@@ -2,8 +2,20 @@ window.onload=init;
 function init(){
 var button=document.getElementById("addButton");
 button.onclick=handleButtonClick;
+loadPlaylist();
 }
 function handleButtonClick(){
-    alert("Button was clicked!");
+    var textInput=document.getElementById("songTextInput");
+    var songName=textInput.value;
+    var li=document.createElement("li");
+    li.innerHTML=songName;
+    var ul=document.getElementById("playlist");
+    ul.appendChild(li);
+    save(songName);
+    if(songName==""){
+        alert("Please enter a song");
+    }
+    else{
+        alert("Adding "+songName);
+    }
 }
-//建立一个处理器处理用户点击事件
